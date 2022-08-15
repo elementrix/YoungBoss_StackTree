32cb479af22514d9ea804ceabab7e73467714efb
김주안 <jooan@gimjuan-ui-MacBookPro.local>
2022-08-16 01:41:15 +0900

    johnny - connect account urls, main urls

---
+++ stacktreeproject/account/urls.py
@@ -0,0 +1,9 @@
+from urllib.parse import urlparse
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/',views.signup, name = "signup"),
+    path('login/',views.login, name = "login"),
+    # path('logout/',views.logout, name = "logout"),
+]
--- stacktreeproject/account/views.py
+++ stacktreeproject/account/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+
+def signup(request):
+
+    return render(request, 'signup.html')
+
+def login(request):
+
+    return render(request, 'login.html')
