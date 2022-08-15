1f89315ac43416705e98e8e7a28bdf7236abe185
김주안 <jooan@gimjuan-ui-MacBookPro.local>
2022-08-15 16:57:29 +0900

    johnny - add static files, add login.html, signup.html, base.html, main.html, css files and collectstatic

--- stacktreeproject/stacktreeproject/settings.py
+++ stacktreeproject/stacktreeproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+    BASE_DIR/'stacktreeproject'/'static',
+    BASE_DIR/'main'/'static',
+    BASE_DIR/'account'/'static',
+]
+
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
