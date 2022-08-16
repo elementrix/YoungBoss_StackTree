413361e89ab5cf81d340b06733bb67c2e054f07a
toby0204 <ekffo0204@kau.kr>
2022-08-16 21:57:22 +0900

    toby modelsetting

--- stacktreeproject/account/models.py
+++ stacktreeproject/account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    User_id=models.AutoField(primary_key=True)
+    Username=models.CharField(max_length=45)
+    Tree_id=ManytoManyField('Tree',blank=True)
+    User_Password=models.CharField(max_length=45)
+    User_Email=models.CharField(max_length=45)
+    User_HP=models.TextField()
