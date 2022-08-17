5a6dc869109cb0b7f28a7c2a37c258d71ec03ab0
toby0204 <ekffo0204@kau.kr>
2022-08-17 20:24:33 +0900

    toby db to model

--- stacktreeproject/account/models.py
+++ stacktreeproject/account/models.py
@@ -1,10 +1,56 @@
+from email.policy import default
+from xmlrpc.client import Boolean
 from django.db import models
 
+
 # Create your models here.
-class User(models.Model):
-    User_id=models.AutoField(primary_key=True)
-    Username=models.CharField(max_length=45)
+
+class User_mastered_syntax(models.Model):
+    User_id=ManytoManyField('User',blank=False)
+    Framework_Syntax_id=ManytoManyField('Framework_Syntax',blank=True)
+    Language_Syntax_id=ManytoManyField('Language_Syntax',blank=True)
+
+class User_to_master_syntax(models.Model):
+    User_id=ManytoManyField('User',blank=False)
+    Framework_Syntax_id=ManytoManyField('Framework_Syntax',blank=True)
+    Language_Syntax_id=ManytoManyField('Language_Syntax',blank=True)
+
+
+class Like(models.Model):
+    User_id=ManytoManyField('User',blank=False)
+    Company_id=ManytoManyField('Company',blank=True)
+    Job_id=ManytoManyField('Job',blank=True)
     Tree_id=ManytoManyField('Tree',blank=True)
-    User_Password=models.CharField(max_length=45)
-    User_Email=models.CharField(max_length=45)
-    User_HP=models.TextField()
+    Framework_id=ManytoManyField('Framework',blank=True)
+    Language_id=ManytoManyField('Language',blank=True)
+
+class Framework_for_tree(models.Model):
+     Framework_id=ManytoManyField('Framework',blank=True)
+     Tree_id=ManytoManyField('Tree',blank=True)
+     order=IntegerField(null=False)
+     essential=BooleanField(default=True)
+
+class Language_for_tree(models.Model):
+     Language_id=ManytoManyField('Language',blank=True)
+     Tree_id=ManytoManyField('Tree',blank=True)
+     order=IntegerField(null=False)
+     essential=BooleanField(default=True)
+
+class Job(models.Model):
+    Job_id=models.AutoField(primary_key=True)
+    Job_name=models.CharField(max_length=45)
+    Tree_id=models.ForeignKey(Tree, on_delete=models.CASCADE)
+    Field_id=models.ForeignKey(Field, on_delete=models.CASCADE)
+    Company_id=models.ForeignKey(Company, on_delete=models.CASCADE)
+
+class Field(models.Model):
+    Field_id=models.AutoField(primary_key=True)
+    Field_name=models.CharField(max_length=45)
+
+class Company(models.Model):
+    Company_id=models.AutoField(primary_key=True)
+    Company_name=models.CharField(max_length=45)
+    Company_Address=models.TextField()
+    Field_id=models.ForeignKey(Field, on_delete=models.CASCADE)
+    Company_description=models.TextField()
+
